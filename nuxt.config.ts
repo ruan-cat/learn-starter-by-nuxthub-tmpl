e81afad42d1e30a3c1fcc9d146a97d70d53b76ea
ruan-cat <1219043956@qq.com>
2024-08-09 17:03:16 +0800

    ⚙️ config: 不提供 blob ；

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-30',
+  compatibilityDate: "2024-07-30",
   // Nuxt 4 directory structure and features
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   // Nuxt Modules
   // https://nuxt.com/modules
-  modules: [
-    '@nuxthub/core',
-    '@nuxt/eslint'
-  ],
+  modules: ["@nuxthub/core", "@nuxt/eslint"],
   hub: {
     database: true,
     kv: true,
-    blob: true,
+    // 不使用blob 不想开通 配置银行卡很麻烦
+    blob: false,
     cache: true,
   },
   nitro: {
     experimental: {
       // Enable Server API documentation within NuxtHub
-      openAPI: true
-    }
+      openAPI: true,
+    },
   },
   // Development
   devtools: { enabled: true },
-})
+});
