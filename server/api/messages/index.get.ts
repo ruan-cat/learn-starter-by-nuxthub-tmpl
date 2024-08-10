5234136c3cceee66746263d73828ccf9a512052c
ruan-cat <1219043956@qq.com>
2024-08-10 21:14:44 +0800

    🤔 save-file: 测试提交，看看云数据库会不会更新表？

--- server/api/messages/index.get.ts
+++ server/api/messages/index.get.ts
@@ -1,10 +1,16 @@
 export default eventHandler(async () => {
-  const db = hubDatabase()
+  const db = hubDatabase();
+
+  console.log(" 看看生产环境会不会有反应？ ");
 
   // TODO: move it a a Server Task
-  await db.exec('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, text TEXT, created_at INTEGER)')
+  await db.exec(
+    "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, text TEXT, created_at INTEGER)"
+  );
 
-  const { results } = await db.prepare('SELECT * FROM messages ORDER BY created_at DESC').all()
+  const { results } = await db
+    .prepare("SELECT * FROM messages ORDER BY created_at DESC")
+    .all();
 
-  return results
-})
+  return results;
+});
