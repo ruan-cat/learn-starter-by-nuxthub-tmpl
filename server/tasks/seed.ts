6bccbdfa08e98667093cb0378e15d32469704087
ruan-cat <1219043956@qq.com>
2024-08-12 16:43:55 +0800

    🎉 init: 提供初始化数据库的数据库任务；

--- server/tasks/seed.ts
+++ server/tasks/seed.ts
@@ -1,14 +1,36 @@
-// export default defineTask({
-// 	meta: {
-// 		name: "db:seed",
-// 		description: "Run database seed task",
-// 	},
-// 	async run() {
-// 		console.log("Running DB seed task...");
+const initProduct: Product[] = [
+	{
+		productId: 1,
+		skuId: 1,
+		sales: 0,
+		completed: 0,
+		createdAt: new Date(),
+	},
+	{
+		productId: 2,
+		skuId: 2,
+		sales: 0,
+		completed: 0,
+		createdAt: new Date(),
+	},
+	{
+		productId: 3,
+		skuId: 3,
+		sales: 0,
+		completed: 0,
+		createdAt: new Date(),
+	},
+];
 
-// 		const initProduct: Product[] = [];
+export default defineTask({
+	meta: {
+		name: "db:seed",
+		description: "Run database seed task",
+	},
+	async run() {
+		console.log("Running DB seed task...");
 
-// 		await useDrizzle().insert(tables.users).values(users);
-// 		return { result: "success" };
-// 	},
-// });
+		await useDrizzle().insert(tables.product).values(initProduct);
+		return { result: "success" };
+	},
+});
