af88001cb63417085785fe452e2efb125fdd22b0
ruan-cat <1219043956@qq.com>
2024-08-15 15:10:31 +0800

    🤔 save-file: 考虑使用 postgresql 实现数据库连接；

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,21 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-	dialect: "sqlite",
 	schema: "./server/database/schema.ts",
 	out: "./server/database/migrations",
 
-	driver: "d1-http",
-	dbCredentials: {
-		accountId: "3412269ab0def154c8806e38acd1b493",
-		databaseId: "22850cc9-7519-4d48-bf90-ea886519f86f",
-		token: "SFJXyE38Aa8qcMfLpCHueuYB4PAgyN_O14d0vkn7",
-	},
+	dialect: "postgresql",
+	// driver: "aws-data-api",
+
+	// dialect: "sqlite",
+	// cloudflare D1
+	// driver: "d1-http",
+	// dbCredentials: {
+	// 	accountId: "3412269ab0def154c8806e38acd1b493",
+	// 	databaseId: "22850cc9-7519-4d48-bf90-ea886519f86f",
+	// 	token: "SFJXyE38Aa8qcMfLpCHueuYB4PAgyN_O14d0vkn7",
+	// },
+
 	migrations: {
 		prefix: "none",
 	},
