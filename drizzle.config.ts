9894629b16f5b5cfd0d39be205d06cc6c18e314c
ruan-cat <1219043956@qq.com>
2024-08-15 17:05:20 +0800

    🦄 refactor: 重构数据库配置；

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,22 +1,58 @@
 import type { Config } from "drizzle-kit";
 
-export default {
+/**
+ * 面向 cloudflare D1 的 drizzle 配置
+ */
+const drizzleConfigWithCloudflareD1: Config = {
 	schema: "./server/database/schema.ts",
 	out: "./server/database/migrations",
 
-	dialect: "postgresql",
-	// driver: "aws-data-api",
-
-	// dialect: "sqlite",
-	// cloudflare D1
-	// driver: "d1-http",
-	// dbCredentials: {
-	// 	accountId: "3412269ab0def154c8806e38acd1b493",
-	// 	databaseId: "22850cc9-7519-4d48-bf90-ea886519f86f",
-	// 	token: "SFJXyE38Aa8qcMfLpCHueuYB4PAgyN_O14d0vkn7",
-	// },
+	dialect: "sqlite",
+	driver: "d1-http",
+	dbCredentials: {
+		accountId: "3412269ab0def154c8806e38acd1b493",
+		databaseId: "22850cc9-7519-4d48-bf90-ea886519f86f",
+		token: "SFJXyE38Aa8qcMfLpCHueuYB4PAgyN_O14d0vkn7",
+	},
 
 	migrations: {
 		prefix: "none",
 	},
-} satisfies Config;
+};
+
+/**
+ * 面向 neon 的 drizzle 配置
+ */
+const drizzleConfigWithNeon: Config = {
+	schema: "./server/database/schema.ts",
+	out: "./server/database/migrations",
+
+	dialect: "postgresql",
+
+	migrations: {
+		prefix: "none",
+	},
+};
+
+export default drizzleConfigWithCloudflareD1;
+
+// export default {
+// 	schema: "./server/database/schema.ts",
+// 	out: "./server/database/migrations",
+
+// 	dialect: "postgresql",
+// 	// driver: "aws-data-api",
+
+// 	// dialect: "sqlite",
+// 	// cloudflare D1
+// 	// driver: "d1-http",
+// 	// dbCredentials: {
+// 	// 	accountId: "3412269ab0def154c8806e38acd1b493",
+// 	// 	databaseId: "22850cc9-7519-4d48-bf90-ea886519f86f",
+// 	// 	token: "SFJXyE38Aa8qcMfLpCHueuYB4PAgyN_O14d0vkn7",
+// 	// },
+
+// 	migrations: {
+// 		prefix: "none",
+// 	},
+// } satisfies Config;
