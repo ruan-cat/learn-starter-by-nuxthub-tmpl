cc93be88c87ccb503a5dd43c076c033ba8a7e355
ruan-cat <1219043956@qq.com>
2024-08-15 17:22:03 +0800

    🦄 refactor: 数据库表名称构造函数；

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,6 +1,19 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer, sqliteTableCreator } from "drizzle-orm/sqlite-core";
+import { dbTableNamePrefix } from "~~/utils";
 
-export const todos = sqliteTable("todos", {
+/** D1 数据表构造器 */
+const cloudflareD1SqliteTable = sqliteTableCreator((name) => `${dbTableNamePrefix}${name}`);
+
+// notuse 不使用 当成一个使用案例
+// export const todos = sqliteTable("todos", {
+// 	id: integer("id").primaryKey(),
+// 	userId: integer("user_id").notNull(), // GitHub Id
+// 	title: text("title").notNull(),
+// 	completed: integer("completed").notNull().default(0),
+// 	createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+// });
+
+export const todos = cloudflareD1SqliteTable("todos", {
 	id: integer("id").primaryKey(),
 	userId: integer("user_id").notNull(), // GitHub Id
 	title: text("title").notNull(),
@@ -8,7 +21,7 @@ export const todos = sqliteTable("todos", {
 	createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
 });
 
-export const product = sqliteTable("product", {
+export const product = cloudflareD1SqliteTable("product", {
 	/** 商品id */
 	productId: integer("product_id").primaryKey(),
 	sales: integer("sales").notNull().default(0),
