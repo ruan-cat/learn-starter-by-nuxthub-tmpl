db0c78f21c469a8e215f70b362d7e5ddaacc2d5f
ruan-cat <1219043956@qq.com>
2024-08-16 09:51:38 +0800

    🐞 fix: hubDatabase 不使用该函数了；不使用和cloudflare高度关联的内容了。

--- server/api/messages/index.get.ts
+++ server/api/messages/index.get.ts
@@ -1,12 +1,8 @@
 export default eventHandler(async () => {
-	const db = hubDatabase();
-
-	console.log(" 看看生产环境会不会有反应？ ");
-
-	// TODO: move it a a Server Task
-	await db.exec("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, text TEXT, created_at INTEGER)");
-
-	const { results } = await db.prepare("SELECT * FROM messages ORDER BY created_at DESC").all();
-
-	return results;
+	// const db = hubDatabase();
+	// console.log(" 看看生产环境会不会有反应？ ");
+	// // TODO: move it a a Server Task
+	// await db.exec("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, text TEXT, created_at INTEGER)");
+	// const { results } = await db.prepare("SELECT * FROM messages ORDER BY created_at DESC").all();
+	// return results;
 });
--- server/utils/cf-d1-db.ts
+++ server/utils/cf-d1-db.ts
@@ -6,7 +6,7 @@ export { sql, eq, and, or } from "drizzle-orm";
 export const tables = schema;
 
 export function useCloudflareD1DB() {
-	return drizzle(hubDatabase(), { schema });
+	// return drizzle(hubDatabase(), { schema });
 }
 
 /**
