3a4e5cc7fa6b65928316f58144aa7c656b29a7ab
ruan-cat <1219043956@qq.com>
2024-08-17 02:03:52 +0800

    🤔 save-file: 从 neon 文档内，提供sql案例；

---
+++ server/database/neon/sql/example.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS playing_with_neon(id SERIAL PRIMARY KEY, name TEXT NOT NULL, value REAL);
+INSERT INTO playing_with_neon(name, value)
+  SELECT LEFT(md5(i::TEXT), 10), random() FROM generate_series(1, 10) s(i);
