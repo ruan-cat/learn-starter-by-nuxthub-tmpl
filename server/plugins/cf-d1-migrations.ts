40c55519b73c79af97b75d5961fb4da62c738906
ruan-cat <1219043956@qq.com>
2024-08-16 09:46:56 +0800

    🐞 fix: onHubReady

--- server/plugins/cf-d1-migrations.ts
+++ server/plugins/cf-d1-migrations.ts
@@ -4,13 +4,14 @@ import { migrate } from "drizzle-orm/d1/migrator";
 export default defineNitroPlugin(async () => {
 	if (!import.meta.dev) return;
 
-	onHubReady(async () => {
-		await migrate(useDB(), { migrationsFolder: "server/database/cloudflare-D1/migrations" })
-			.then(() => {
-				consola.success("Database migrations done");
-			})
-			.catch((err) => {
-				consola.error("Database migrations failed", err);
-			});
-	});
+	// 不使用 nuxthub 和 cloudflare D1 了。
+	// onHubReady(async () => {
+	// 	await migrate(useDB(), { migrationsFolder: "server/database/cloudflare-D1/migrations" })
+	// 		.then(() => {
+	// 			consola.success("Database migrations done");
+	// 		})
+	// 		.catch((err) => {
+	// 			consola.error("Database migrations failed", err);
+	// 		});
+	// });
 });
